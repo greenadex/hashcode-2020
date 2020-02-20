8045c69f4c3097f0be03d1d061c8cdcbd4bf636e
Iulia Harasim <iulia.harasim@gmail.com>
2020-02-20 19:27:22 +0200

    add file read

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const string prefix = "";
+const string input_file = prefix + ".in";
+const string output_file = prefix + ".out";
+
+ifstream fin("/home/iulia/Downloads/" + input_file);
+ofstream fout("/home/iulia/CLionProjects/hashcode/" + output_file);
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  cout << "Hello, World!" << endl;
   return 0;
 }
