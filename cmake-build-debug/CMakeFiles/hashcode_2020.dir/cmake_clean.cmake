679841b76931f3a71f6ee9dbcc67dec95662c960
Iulia Harasim <iulia.harasim@gmail.com>
2020-02-20 19:19:49 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/hashcode_2020.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hashcode_2020.dir/main.cpp.o"
+  "hashcode_2020.pdb"
+  "hashcode_2020"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hashcode_2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/hashcode_2020.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hashcode_2020.
+# This may be replaced when dependencies are built.
